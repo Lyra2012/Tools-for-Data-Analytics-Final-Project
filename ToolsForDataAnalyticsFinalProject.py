2bcdcf42e3d6ee228a3418769d3f85f7dc9b8255
Lyra2012 <126820247+Lyra2012@users.noreply.github.com>
2023-03-16 15:20:44 -0500

    Add files via upload

---
+++ ToolsForDataAnalyticsFinalProject.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Tools For Data Science Final Project
+
+# #### This is an introduction to the final project for the second module of the IBM Data Science Professional Certificate
+
+# ##### Some of the Most Popular Data Science Languages include: 
+# 1. Python
+# 2. R 
+# 3. SQL
+# 4. JavaScript
+# 5. Java
+# 6. C/C++
+# 7. MATLAB
+# 8. Scala
+# 9. Julia
+# 10. SAS
+
+# ##### Some of the Most Popular Data Science Libraries include:
+# 1. Numpy for scientific computing and performing basic and advanced array operations
+# 2. SciPy for scientific programming projects and great for numerical routines such as numerical optimizations, integrations, and others in submodules
+# 3. Pandas for work with labeled or relational data  and allows for the converting of data structures into DataFrame objects, handling missing data, adding/deleting objects from the DataFrame, imputing missing files, and plotting data
+# 4. Keras for building neural networks and modeling
+# 5. Sci-Kit Learn for Data Science projects based in Python
+# 6. PyTorch for data scientists who want to perform deep learning tasks
+# 7. TensorFlow for machine learning and deep learning
+# 8. Matplotlib for for generating data visualizations of two-dimensional diagrams and graphs
+# 9. Scrapy for building crawling programs that retrieve structured data from the web
+
+# |Data |Science |Tools |
+# |------------------|:------------------:|----------------:|
+# | Jupyter Notebooks|MATLAB              |Excel            |
+# |Apache Spark      |Tableau             |Python           |
+
+# 1+1=2; 1+2=3; 1+3=4; 1+4=5...
+
+# In[ ]:
+
+
+1+2+2*5
+
+
+# In[31]:
+
+
+def convert(hours):
+    hours = minutes // 60
+    minutes = 340
+
+
+# Objectives of this Assignment Included: 
+# 1. Markdown Cells
+# 2. Lists
+# 3. Tables
+# 4. Cells
+# 5. Math
+# 6. Code
+
+# By: Laura Hultquist
